a04b8769632d895f88d68bcb49895e74b243cb1b
bogdantro <sabertoothtri@gmail.com>
2023-06-25 16:25:14 +0200

    sd

--- apps/core/models.py
+++ apps/core/models.py
@@ -3,3 +3,11 @@ from datetime import date
 from django.contrib.auth.models import User
 from django.utils import tree
 
+class Results(models.Model):
+    name = models.CharField(max_length=300, blank=True, null=True)
+    user_image = models.ImageField(blank=True, default='', upload_to='other/user_images/')
+    place = models.CharField(max_length=300, blank=True, null=True)
+    points = models.CharField(max_length=300, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
--- static/js/home.js
+++ static/js/home.js
@@ -1,20 +1,76 @@
 function hallOfFame(){
-    document.getElementById('hallOfFameBtn').classList.add('active');
-
-    document.getElementById('competitionBtn').classList.remove('active');
-    document.getElementById('historyBtn').classList.remove('active');
+    document.getElementById('hallOfFameChoices').classList.add('active');
 }
+
+function hallOfFameDay(){
+    document.getElementById('hallOfFameDaySec').classList.add('active');
+    
+    document.getElementById('hallOfFameChoices').classList.remove('active');
+
+    document.getElementById('hallOfFameWeekSec').classList.remove('active');
+    document.getElementById('hallOfFameMonthSec').classList.remove('active');
+    document.getElementById('hallOfFameYearSec').classList.remove('active');
+    document.getElementById('hallOfFameAllTimeSec').classList.remove('active');
+}
+
+function hallOfFameWeek(){
+    document.getElementById('hallOfFameWeekSec').classList.add('active');
+
+    document.getElementById('hallOfFameChoices').classList.remove('active');
+
+    document.getElementById('hallOfFameDaySec').classList.remove('active');
+    document.getElementById('hallOfFameMonthSec').classList.remove('active');
+    document.getElementById('hallOfFameYearSec').classList.remove('active');
+    document.getElementById('hallOfFameAllTimeSec').classList.remove('active');
+}
+
+function hallOfFameMonth(){
+    document.getElementById('hallOfFameMonthSec').classList.add('active');
+
+    document.getElementById('hallOfFameChoices').classList.remove('active');
+
+    document.getElementById('hallOfFameWeekSec').classList.remove('active');
+    document.getElementById('hallOfFameDaySec').classList.remove('active');
+    document.getElementById('hallOfFameYearSec').classList.remove('active');
+    document.getElementById('hallOfFameAllTimeSec').classList.remove('active');
+}
+
+function hallOfFameYear(){
+    document.getElementById('hallOfFameYearSec').classList.add('active');
+
+    document.getElementById('hallOfFameChoices').classList.remove('active');
+
+    document.getElementById('hallOfFameWeekSec').classList.remove('active');
+    document.getElementById('hallOfFameMonthSec').classList.remove('active');
+    document.getElementById('hallOfFameDaySec').classList.remove('active');
+    document.getElementById('hallOfFameAllTimeSec').classList.remove('active');
+}
+
+function hallOfFameAllTime(){
+    document.getElementById('hallOfFameAllTimeSec').classList.add('active');
+
+    document.getElementById('hallOfFameChoices').classList.remove('active');
+
+    document.getElementById('hallOfFameWeekSec').classList.remove('active');
+    document.getElementById('hallOfFameMonthSec').classList.remove('active');
+    document.getElementById('hallOfFameYearSec').classList.remove('active');
+    document.getElementById('hallOfFameDaySec').classList.remove('active');
+}
+
+function hallOfFameClose(){
+    document.getElementById('hallOfFameChoices').classList.remove('active');
+}
+
+//
 
 function competition(){
     document.getElementById('competitionBtn').classList.add('active');
-
-    document.getElementById('hallOfFameBtn').classList.remove('active');
-    document.getElementById('historyBtn').classList.remove('active');
 }
+
+
+
+// 
 
 function history(){
     document.getElementById('historyBtn').classList.add('active');
-
-    document.getElementById('hallOfFameBtn').classList.remove('active');
-    document.getElementById('competitionBtn').classList.remove('active');
 }
